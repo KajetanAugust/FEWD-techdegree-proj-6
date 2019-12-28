12a34663f086ad9a839ac25eedf21260064af6ed
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2019-12-28 16:17:15 +0100

    Adding comments to js code

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let missed = 0;
 const keyboard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 
+// ARRAY OF PHRASES
+
 const phrases = [
     'Right Out of the Gate',
     'Raining Cats and Dogs',
@@ -19,48 +21,48 @@ const phrases = [
 //RANDOM PHRASE GENERATOR
 
 function randomPhrase() {
-    const highest = (phrases.length);
-    const draw = Math.floor( Math.random() * highest);
-    return phrases[draw];
+    const highest = (phrases.length); // Const contains the highest number for draw
+    const draw = Math.floor( Math.random() * highest); // Math.random draws the random number from 0 to const highest
+    return phrases[draw]; // the array item is returned
 };
 
 //CREATING ARRAY OF LETTERS
 
 function getRandomPhraseAsArray(){
-    const arrayOfLetters = randomPhrase().split('')
+    const arrayOfLetters = randomPhrase().split('') // the phrase chosen in randomPhrase function is split to array of letters
     
-    return arrayOfLetters;
+    return arrayOfLetters; // array of letters is returned
 }
 
 
 //PHRASE TO DISPLAY
 
 function addPhraseToDisplay() {
-        const phraseToProcess = getRandomPhraseAsArray()
-        let arrayToDisplay = '';
-    for(let i = 0; i < phraseToProcess.length; i++)  {
-        arrayToDisplay += '<li>' + phraseToProcess[i] + '</li>';
+        const phraseToProcess = getRandomPhraseAsArray() // getRandomPhraseAsArray function is called and assigned to the const
+        let arrayToDisplay = ''; // empty variable for li items
+    for(let i = 0; i < phraseToProcess.length; i++)  { 
+        arrayToDisplay += '<li>' + phraseToProcess[i] + '</li>'; // for loop loops through the array and puts every element in li tag
     }
     return arrayToDisplay;
 }
-const finalPhrase = document.querySelector('#phrase ul')
-finalPhrase.innerHTML = addPhraseToDisplay();
+const finalPhrase = document.querySelector('#phrase ul') // ul is selected
+finalPhrase.innerHTML = addPhraseToDisplay(); // li items are added to the ul inside the .phrases div
 
 // ADDING CLASS
 
-let phraseLi = document.querySelectorAll('ul li');
-for (let i = 0; i < phraseLi.length; i ++ ){
-    if(phraseLi[i].textContent !== ' '){
-        phraseLi[i].className = 'letter';
+let phraseLi = document.querySelectorAll('ul li'); // li elements are selected
+for (let i = 0; i < phraseLi.length; i ++ ){ // for loop loops through the li elements
+    if(phraseLi[i].textContent !== ' '){ // if statement checks if textContent of li elements is not a space
+        phraseLi[i].className = 'letter'; // if textContent is not a space it gets the class 'letter'
     } else {
-        phraseLi[i].className = 'space';
+        phraseLi[i].className = 'space'; // if textContent is a space it gets the class space
     };
 }
 
 // START BUTTON
 
-const startButton = document.querySelector('#overlay a');
-const startScreen = document.getElementById('overlay');
-startButton.addEventListener('click', () => {
-    startScreen.style.display = 'none';
+const startButton = document.querySelector('#overlay a'); // the start button is selected
+const startScreen = document.getElementById('overlay'); // the start screen overlay is selected
+startButton.addEventListener('click', () => { // event listener is added to the startButton
+    startScreen.style.display = 'none'; // if the startButton is clicked it changes the display of startScreen to none
 });
